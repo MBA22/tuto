9aa17eb99d612048b8df249c49952233398491ee
MBA22 <Mario Barragan>
2015-03-27 20:31:42 -0600

    first commit

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,10 @@ module ProductsHelper
 	def print_price(price)
 	  number_to_currency (price)
 	end
+
+	def print_stock(stock)
+	    text = stock > 0 ? "In Stock (#{stock})" : "Out Of stock"
+	    css = stock > 0 ? "in_stock" : "out_stock"
+	    content_tag :span, text , class:[css]
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	has_many :order_items
+	validates_numericality_of :price
+
+	def price=(input)
+	  input.delete!("$")
+	  super
+	end
 end
